f55b656fb1ec8d8e67edf3003ce7fb7c1fa7d1ba
teamFiveCapstone <Bajcarzach@icloud.com>
2025-11-03 16:21:34 -0500

    input output added

--- README.md
+++ README.md
@@ -6,8 +6,13 @@ npm install @langchain/core@latest @langchain/community @langchain/textsplitters
 
 2.) Add "type": "module" to package.json
 
-3.) Run
+3.) Create a input and output folder and place your pdf in the input folder
+
+4.) Run
 
 ```
 node main.js
 ```
+
+5.) View the output in output folder
+//langchain seems to have recursive text splitter, token splitter or text splitter all of which you can change the chunk size and overlap. Doesn't have sentence or paragraph though
--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import fs from "fs";
+import path from "path";
+
+const fileNames = ["lion-quick-guide", "ANAC+2024+Digital"];
+const fileName = fileNames[0];
 
 async function main() {
-  const loader = new PDFLoader("lion-quick-guide.pdf");
+  const inputFile = path.resolve(`./input/${fileName}.pdf`);
+  const loader = new PDFLoader(inputFile, { splitPages: false });
   const docs = await loader.load();
 
   const splitter = new RecursiveCharacterTextSplitter({
     chunkSize: 1000,
-    chunkOverlap: 100,
+    chunkOverlap: 200,
   });
   const chunks = await splitter.splitDocuments(docs);
+
+  const outputFile = path.resolve(`./output/${fileName}.txt`);
+  const separator = "\n------------------------------\n";
+  const allText = chunks.map((chunk) => chunk.pageContent).join(separator);
+
+  fs.writeFileSync(outputFile, allText, "utf8");
+
+  console.log("Chunks are saved in the output folder!");
 }
 
 main();
